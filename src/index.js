e36bdd38afb954645034be413e01e15b20376c6e
Karol <kpulawski13@gmail.com>
2023-08-02 09:50:28 +0100

    refractored weather.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './styles/style.css';
 import { getWeather } from './weather';
 
-getWeather('sandomierz').catch((error) => console.log(error));
+getWeather('s').catch((error) => console.log(error));
--- src/weather.js
+++ src/weather.js
@@ -1,3 +1,5 @@
+let threeDaysForecastData = [];
+
 function getUserLocation() {
   return new Promise((resolve, reject) => {
     if ('geolocation' in navigator) {
@@ -8,32 +10,52 @@ function getUserLocation() {
     } else {
       reject(new Error('Geolocation is not supported by this browser'));
     }
-  }).catch((error) => console.log(error));
+  });
+}
+
+function filterAndTransformForecast(day) {
+  const { sunrise, sunset, moon_phase: moonPhase } = day.astro;
+  const {
+    maxtemp_c: maxTempC,
+    mintemp_c: minTempC,
+    mintemp_f: minTempF,
+    maxtemp_f: maxTempF,
+    avghumidity: humidity,
+  } = day.day;
+  const { text: weatherDescription, icon } = day.day.condition;
+  const weatherInfo = {
+    sunrise,
+    sunset,
+    maxTempC,
+    minTempC,
+    minTempF,
+    maxTempF,
+    weatherDescription,
+    icon,
+    moonPhase,
+    humidity,
+  };
+  return threeDaysForecastData.push(weatherInfo);
 }
 
 function getWeather(location) {
-  let url = ``;
   let latitude;
   let longitude;
-
+  let url = '';
+  // try to get user location if no location provided
   getUserLocation()
     .then((response) => {
       latitude = response.coords.latitude;
       longitude = response.coords.longitude;
-      if (latitude && longitude) {
-        url = `http://api.weatherapi.com/v1/forecast.json?key=a92646fcecc1405c93e80851233007&q=${latitude},${longitude}&days=3`;
-      }
+      url = `http://api.weatherapi.com/v1/forecast.json?key=a92646fcecc1405c93e80851233007&q=${latitude},${longitude}&days=3`;
     })
     .catch((error) => {
-      console.error('Error getting user location:', error);
+      console.error('Error getting user location:', error.message);
     })
     .then(() => {
-      if (!url) {
-        if (location) {
-          url = `http://api.weatherapi.com/v1/forecast.json?key=a92646fcecc1405c93e80851233007&q=${location}&days=3`;
-        } else
-          url = `http://api.weatherapi.com/v1/forecast.json?key=a92646fcecc1405c93e80851233007&q=&days=3`;
-      }
+      if (location) {
+        url = `http://api.weatherapi.com/v1/forecast.json?key=a92646fcecc1405c93e80851233007&q=${location}&days=3`;
+      } else throw new Error('No location provided');
       return fetch(url, { mode: 'cors' });
     })
     .then((response) => {
@@ -44,38 +66,16 @@ function getWeather(location) {
     })
     .then((response) => {
       const { country, name, region } = response.location;
-      const threeDaysForecast = [];
       const forecast = response.forecast.forecastday;
-      forecast.forEach((day) => {
-        const { sunrise, sunset, moon_phase: moonPhase } = day.astro;
-        const {
-          maxtemp_c: maxTempC,
-          mintemp_c: minTempC,
-          mintemp_f: minTempF,
-          maxtemp_f: maxTempF,
-          avghumidity: humidity,
-        } = day.day;
-        const { text: weatherDescription, icon } = day.day.condition;
-        const weatherInfo = {
-          sunrise,
-          sunset,
-          maxTempC,
-          minTempC,
-          minTempF,
-          maxTempF,
-          weatherDescription,
-          icon,
-          moonPhase,
-          humidity,
-        };
-        return threeDaysForecast.push(weatherInfo);
-      });
+      threeDaysForecastData = [];
+      forecast.forEach(filterAndTransformForecast);
       console.log(country, name, region);
-      console.log(threeDaysForecast);
+      console.log(threeDaysForecastData);
       console.log(response);
     })
     .catch((error) => {
-      console.error('Failed to fetch:', error);
+      console.error('Failed to fetch:', error.message);
     });
 }
+
 export { getWeather, getUserLocation };
